b36dfad6af10dd59008e579d2ef1fb41fd28db98
Jijovo <jijovo77@gmail.com>
2026-05-20 13:59:40 -0700

    milestone 1

--- main.cpp
+++ main.cpp
@@ -1,8 +1,29 @@
 //Ezzat Mohamadein | ComSc 210 | Final 1
 #include <iostream>
+#include <map>
+#include <string>
+#include <fstream>
 using namespace std;
 
 int main() {
+    //Milestone 1, create std::map to store airport codes paired with traffic count (number of times it is listed in file)
+    map<string, int> airportMap;
+    
+    string line;
+    ifstream fin("210-final-1-SP26.txt");
+    while (fin >> line) {
+        if(airportMap.find(line) == airportMap.end()) {
+            airportMap[line] = 1;
+        }
+        else {
+            airportMap[line] += 1;
+        }
+    }
+    fin.close();
+
+    for (auto it = airportMap.begin(); it != airportMap.end(); it++) {
+        cout << it->first << " " << it->second << endl;
+    }
 
     return 0;
 }
